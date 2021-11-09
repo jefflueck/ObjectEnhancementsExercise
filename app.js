49bf01d1ffcedfdf45e64803187c4af60ddfd73d
jefflueck <lueckcfd@yahoo.com>
2021-11-08 21:56:46 -0600

    current progress

---
+++ app.js
@@ -0,0 +1,67 @@
+// function createInstructor(firstName, lastName) {
+//   return {
+//     firstName: firstName,
+//     lastName: lastName,
+//   };
+// }
+
+/* Write an ES2015 Version */
+function createInstructor(firstName, lastName) {
+  return {
+    firstName,
+    lastName,
+  };
+}
+
+// var favoriteNumber = 42;
+
+// var instructor = {
+//   firstName: 'Colt',
+// };
+
+// instructor[favoriteNumber] = 'That is my favorite!';
+
+/* Write an ES2015 Version */
+// const instructor = {
+//   fistName: 'Colt',
+//   [favoriteNumber]: 'This is my favorite',
+// };
+
+// var instructor = {
+//   firstName: 'Colt',
+//   sayHi: function () {
+//     return 'Hi!';
+//   },
+//   sayBye: function () {
+//     return this.firstName + ' says bye!';
+//   },
+// };
+
+/* Write an ES2015 Version */
+// let instructor = {
+//   fistName: 'Colt',
+//   sayHi() {
+//     return 'Hi';
+//   },
+//   sayBye() {
+//     return this.fistName + ' says bye!';
+//   },
+// };
+
+const d = createAnimal('dog', 'bark', 'Woooof!');
+// {species: "dog", bark: ƒ}
+d.bark(); //"Woooof!"
+
+const s = createAnimal('sheep', 'bleet', 'BAAAAaaaa');
+// {species: "sheep", bleet: ƒ}
+s.bleet(); //"BAAAAaaaa"
+
+// close
+function createAnimal(species, verb, noise) {
+  return {
+    species,
+  },
+  makeNoise(verb){
+    [verb]noise
+  }
+}
